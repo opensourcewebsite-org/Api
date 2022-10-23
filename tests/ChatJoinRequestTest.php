a59ebdd2c15cb58dc05521b18fa8afd3b424d9f2
bakteriachan <josuegarcia17904@gmail.com>
2022-10-23 06:06:30 +0000

    added ChatJoinRequest Type and tests

---
+++ tests/ChatJoinRequestTest.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace TelegramBot\Api\Test;
+
+use PHPUnit\Framework\TestCase;
+use TelegramBot\Api\Types\Chat;
+use TelegramBot\Api\Types\ChatInviteLink;
+use TelegramBot\Api\Types\ChatJoinRequest;
+use TelegramBot\Api\Types\User;
+
+class ChatjoinRequestTest extends TestCase
+{
+    public function testGetChat()
+    {
+        $item = new ChatJoinRequest();
+        $item->setChat(Chat::fromResponse([
+            'id' => 255,
+            'type' => 'private',
+        ]));
+        $this->assertEquals(
+            Chat::fromResponse([
+                'id' => 255,
+                'type' => 'private',
+            ]),
+            $item->getChat(),
+        );
+    }
+
+    public function testSetChat()
+    {
+        $item = new ChatJoinRequest();
+        $chat = Chat::fromResponse([
+            'id' => 512,
+            'type' => 'private',
+        ]);
+        $item->setChat($chat);
+        $this->assertEquals(
+            Chat::fromResponse([
+                'id' => 512,
+                'type' => 'private',
+            ]),
+            $item->getChat(),
+        );
+    }
+
+    public function testGetFrom()
+    {
+        $item = new ChatJoinRequest();
+        $item->setFrom(User::fromResponse([
+            'id' => 512,
+            'first_name' => 'bakteria',
+        ]));
+        $this->assertEquals(
+            User::fromResponse([
+                'id' => 512,
+                'first_name' => 'bakteria',
+            ]),
+            $item->getFrom(),
+        );
+    }
+
+    public function testSetFrom()
+    {
+        $item = new ChatJoinRequest();
+        $user = User::fromResponse([
+            'id' => 255,
+            'first_name' => 'bakteria',
+        ]);
+        $item->setFrom($user);
+        $this->assertEquals(
+            User::fromResponse([
+                'id' => 255,
+                'first_name' => 'bakteria'
+            ]),
+            $item->getFrom(),
+        );
+    }
+
+    public function testGetDate()
+    {
+        $item = new ChatJoinRequest();
+        $item->setDate(512);
+        $this->assertEquals(512, $item->getDate());
+    }
+
+    public function testSetDate()
+    {
+        $item = new ChatJoinRequest();
+        $item->setDate(255);
+        $this->assertEquals(255, $item->getDate());
+    }
+
+    public function testGetBio()
+    {
+        $item = new ChatJoinRequest();
+        $item->setBio("a telegram bio");
+        $this->assertEquals(
+            "a telegram bio",
+            $item->getBio(),
+        );
+    }
+
+    public function testSetBio()
+    {
+        $item = new ChatJoinRequest();
+        $item->setBio("a telegram bio");
+        $this->assertEquals(
+            "a telegram bio",
+            $item->getBio(),
+        );
+    }
+
+    public function testGetInviteLink()
+    {
+        $item = new ChatJoinRequest();
+        $link = ChatInviteLink::fromResponse([
+            'invite_link' => 'http://url.com',
+            'creator' =>  [
+                'id' => 512,
+                'first_name' => 'bakteria',
+            ],
+        ]);
+        $item->setInviteLink($link);
+        $this->assertEquals(
+            ChatInviteLink::fromResponse([
+                'invite_link' => 'http://url.com',
+                'creator' => [
+                    'id' => 512,
+                    'first_name' => 'bakteria',
+                ],
+            ]),
+            $item->getInviteLink(),
+        );
+    }
+
+    public function testSetInviteLink()
+    {
+        $item = new ChatJoinRequest();
+        $link = ChatInviteLink::fromResponse([
+            'invite_link' => 'http://url.com',
+            'creator' =>  [
+                'id' => 512,
+                'first_name' => 'bakteria',
+            ],
+        ]);
+        $item->setInviteLink($link);
+        $this->assertEquals(
+            ChatInviteLink::fromResponse([
+                'invite_link' => 'http://url.com',
+                'creator' => [
+                    'id' => 512,
+                    'first_name' => 'bakteria',
+                ],
+            ]),
+            $item->getInviteLink(),
+        );
+    }
+}
