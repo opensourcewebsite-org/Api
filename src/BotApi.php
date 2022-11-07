84547ccb30f235cd5e235b10ac1282ea69287cce
bakteriachan <josuegarcia17904@gmail.com>
2022-11-07 18:02:19 +0000

    added editForumTopic method

--- src/BotApi.php
+++ src/BotApi.php
@@ -1811,6 +1811,26 @@ class BotApi
     }
 
     /**
+     * Use this method to edit name and icon of a topic in a forum supergroup chat. The bot must be an administrator in the chat for this to work and must have can_manage_topics administrator rights, unless it is the creator of the topic. Returns True on success
+     *
+     * @param integer|string $chatId Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
+     * @param integer $messageThreadId Unique identifier for the target message thread of the forum topic
+     * @param string $name New topic name, 1-128 characters
+     * @param string $iconCustomEmojiId New unique identifier of the custom emoji shown as the topic icon. Use getForumTopicStickers to get all allowed custom emoji identifiers.
+     *
+     * @return bool
+     */
+    public function editForumTopic($chatId, $messageThreadId, $name, $iconCustomEmojiId)
+    {
+        return $this->call('editForumTopic', [
+            'chat_id' => $chatId,
+            'name' => $name,
+            'message_thread_id' => $messageThreadId,
+            'icon_custom_custom_emoji_id' => $iconCustomEmojiId,
+        ]);
+    }
+
+    /**
      * Use this method for your bot to leave a group, supergroup or channel.
      *
      * @param string|int $chatId Unique identifier for the target chat or username of the target channel
