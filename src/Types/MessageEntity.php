dc4a0821fc75e4a21ca4c796d138854bbc875fc1
Vitaliy Shevchenko <grandmotivator@gmail.com>
2022-12-27 10:39:13 +0700

    add comments

--- src/Types/MessageEntity.php
+++ src/Types/MessageEntity.php
@@ -55,10 +55,11 @@ class MessageEntity extends BaseType implements TypeInterface
 
     /**
      * Type of the entity.
-     * One of mention (@username), hashtag (#hashtag), cashtag ($USD), bot_command, url, email, phone_number,
-     * bold (bold text), italic (italic text), underline (underlined text), strikethrough (strikethrough text),
-     * code (monowidth string), pre (monowidth block), text_link (for clickable text URLs),
-     * text_mention (for users without usernames)
+     * Currently, can be “mention” (@username), “hashtag” (#hashtag), “cashtag” ($USD), “bot_command” (/start@jobs_bot),
+     * “url” (https://telegram.org), “email” (do-not-reply@telegram.org), “phone_number” (+1-212-555-0123),
+     * “bold” (bold text), “italic” (italic text), “underline” (underlined text), “strikethrough” (strikethrough text),
+     * “spoiler” (spoiler message), “code” (monowidth string), “pre” (monowidth block), “text_link” (for clickable text URLs),
+     * “text_mention” (for users without usernames), “custom_emoji” (for inline custom emoji stickers)
      *
      * @var string
      */
