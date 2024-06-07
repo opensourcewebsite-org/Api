56309db6f43da0ba823a78a3ec42683169baeed3
Vitaliy Shevchenko <grandmotivator@gmail.com>
2024-06-07 12:02:47 +0700

    add declineChatJoinRequest

--- src/BotApi.php
+++ src/BotApi.php
@@ -2141,6 +2141,24 @@ class BotApi
     }
 
     /**
+     * Use this method to decline a chat join request. The bot must be an administrator in the chat for this to work and must have the can_invite_users administrator right. Returns True on success.
+     *
+     * @param integer $chatId Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
+     * @param $userId
+     * @return bool
+     * @throws Exception
+     * @throws HttpException
+     * @throws InvalidJsonException
+     */
+    public function declineChatJoinRequest($chatId, $userId)
+    {
+        return $this->call('declineChatJoinRequest', [
+            'chat_id' => $chatId,
+            'user_id' => $userId,
+        ]);
+    }
+
+    /**
      * Use this method to edit the name of the 'General' topic in a forum supergroup chat. The bot must be an administrator in the chat for this to work and must have can_manage_topics administrator rights. Returns True on success.
      *
      * @param integer $chatId Unique identifier for the target chat or username of the target supergroup (in the format @supergroupname )
